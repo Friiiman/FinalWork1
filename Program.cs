4e915382106fb59061506f9dc9362d1c3fdb5c1b
Friiiman <friiiman94@gmail.com>
2022-09-05 15:44:02 +0300

    добавил решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//--------ЗАДАЧА--------
+
+void Task(){
+    string[] arrayFirst = {"qwert", "asd", "12", "9876", ":-)"};
+    Console.WriteLine("Первоначальный массив");
+    PrintArray(arrayFirst);
+
+    string fill = string.Empty;
+    fill = SortSymbol(arrayFirst, fill);
+
+    string[] arraySecond = {fill};
+    Console.WriteLine("Изменённый массив");
+    PrintArray(arraySecond);
+}
+
+//--------МЕТОДЫ--------
+
+void PrintArray(string[] array){
+    int size = array.Length;
+    for(int i = 0; i < size; i++){
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write("\b\b ");
+    Console.WriteLine();
+}
+
+string SortSymbol(string[] array, string fill){
+    int size = array.Length;
+    for(int i = 0; i < size; i++){
+        if(array[i].Length <= 3){
+            fill += array[i] + ", ";
+        }
+    }
+    fill = fill.Remove(fill.Length - 2);
+    return fill;
+}
+
+//--------ВЫВОД РЕШЕНИЯ--------
+
+Task();
